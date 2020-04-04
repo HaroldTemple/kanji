c926778d86299bd3f76e3caa3d13a77e354075d9
HaroldTemple <40338759+HaroldTemple@users.noreply.github.com>
2020-04-05 01:42:44 +0200

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,47 +1,38 @@
+# Moving binaries
 mv ./bin/helm /usr/bin/
-
 mv ./bin/k9s /usr/bin/
-
 mv ./bin/kind /usr/bin/
 
+# Docker - Remove 
 apt-get remove docker docker-engine docker.io containerd runc
 
+# Docker - Install 
 apt-get update
-
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common -y
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
 apt-key fingerprint 0EBFCD88
-
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-
 apt-get update
-
 apt-get install docker-ce docker-ce-cli containerd.io -y
-
 apt-cache madison docker-ce
-
 apt-get install docker-ce=<VERSION_STRING> docker-ce-cli=<VERSION_STRING> containerd.io
 
+# Kubectl - Install
 apt-get update && sudo apt-get install -y apt-transport-https
-
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-
 apt-get update
-
 apt-get install -y kubectl
 
+# Ingress - Create Cluster
 cat <<EOF | kind create cluster --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
@@ -63,14 +54,13 @@ nodes:
     protocol: TCP
 EOF
 
+# Ingress - Apply patches
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/mandatory.yaml
-
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/provider/baremetal/service-nodeport.yaml
-
 kubectl patch deployments -n ingress-nginx nginx-ingress-controller -p '{"spec":{"template":{"spec":{"containers":[{"name":"nginx-ingress-controller","ports":[{"containerPort":80,"hostPort":80},{"containerPort":443,"hostPort":443}]}],"nodeSelector":{"ingress-ready":"true"},"tolerations":[{"key":"node-role.kubernetes.io/master","operator":"Equal","effect":"NoSchedule"}]}}}}'
 
+# APP Kanji - Clone repo
 git clone https://github.com/frmadem/kanji_chart.git
 
-cd kanji_chart
-
-helm install kanji . -f values.yaml
+# Kubernetes - Launch Handbasket
+helm install kanji ./kanji_chart/ -f ./kanji_chart/values.yaml
